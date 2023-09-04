c468f0d1c88b52edb6999c1b774e89883c5b485b
oghene-ella <oghenekaro57@gmail.com>
2023-09-04 22:53:51 +0100

    made some changes to some table's properties

--- oghenekaro_emmanuellla.sql
+++ oghenekaro_emmanuellla.sql
@@ -6,7 +6,7 @@
 -- Create all tables
 -- CREATE TABLE clothingDB.clothing_item (clothing_item_id varchar(7), clothing_item_name varchar(255), clothing_item_description varchar (255), clothing_item_price integer, clothing_item_category varchar(255), clothing_item_size varchar(255), PRIMARY KEY (clothing_item_id));
 
--- CREATE Table clothingDB.admin (admin_id varchar(7), admin_role varchar(255), admin_status boolean, user_id varchar(7), PRIMARY KEY (admin_id), FOREIGN KEY (user_id) REFERENCES user(user_id));
+-- CREATE Table clothingDB.admin (admin_id varchar(7), admin_role ENUM ("super_admin", "content_admin"), is_active boolean, user_id varchar(7), PRIMARY KEY (admin_id), FOREIGN KEY (user_id) REFERENCES user(user_id));
 
 -- CREATE TABLE clothingDB.customer_order (customer_order_id varchar(7), customer_location varchar(255), customer_amount_spent integer, order_created timestamp, payment_method_id varchar(7), clothing_item_id varchar(7), delivery_id varchar(7), PRIMARY KEY (customer_order_id), FOREIGN KEY (payment_method_id) REFERENCES payment_method(payment_method_id), FOREIGN KEY (clothing_item_id) references clothing_item(clothing_item_id), FOREIGN KEY (delivery_id) references delivery_method(delivery_id));
 
@@ -27,16 +27,27 @@
 -- SHOW TABLES
 
 
+-- Rename some columns
+-- Rename a column in user table
+-- ALTER TABLE user
+   -- CHANGE COLUMN user_role users_role  
+	-- ENUM ("admin", "cashier", "customer");
+
 -- Rename a column in payment_method table
 -- ALTER TABLE payment_method
-  -- CHANGE COLUMN payment_method_type is_transfer
-   -- boolean NOT NULL;
+   -- CHANGE COLUMN is_transfer payment_method_type  
+	-- ENUM ("transfer", "cash", "debit_card");
    
 -- Rename a column in cashier_payroll table
 -- ALTER TABLE cashier_payroll
-   -- CHANGE COLUMN payroll_type is_monthly
-    -- boolean NOT NULL;
--- SELECT * FROM cashier_payroll
+   -- CHANGE COLUMN is_monthly payroll_type
+    -- ENUM ("daily", "weekly", "monthly");
+
+-- Rename a column in delivery_method table
+ -- ALTER TABLE delivery_method
+    -- CHANGE COLUMN is_doorstep delivery_type  
+	 -- ENUM ("doorstep", "shop outlet", "pickup station");
+
 
 -- Inserting Records for all entites (tables)
 
@@ -48,7 +59,56 @@
 -- SELECT * FROM clothing_item ;
 
 
+-- Inserting record for admin table
+-- INSERT INTO clothingDB.admin VALUES ("admin01", "super_admin", 1, 
+-- 	(SELECT user_id FROM user WHERE user_id = "user_52")
+--     );
+
+-- INSERT INTO clothingDB.admin VALUES ("admin02", "content_admin", 0, 
+-- 	(SELECT user_id FROM user WHERE user_id = "user_28")
+--     );
+
+-- INSERT INTO clothingDB.admin VALUES ("admin03", "content_admin", 1, 
+-- 	(SELECT user_id FROM user WHERE user_id = "user_71")
+--     );
+-- show items in admin table
+-- SELECT * FROM admin ;
+
+
+-- Inserting record for customer table [todo]
+-- INSERT INTO clothingDB.customer VALUES ("cus_A01", "ord_01", 
+--  (SELECT user_id FROM user WHERE user_id="user_07")
+-- );
+
+-- INSERT INTO clothingDB.customer VALUES ("cus_A02", "ord_02", 
+-- (SELECT user_id FROM user WHERE user_id="user_90")
+-- );
+
+-- INSERT INTO clothingDB.customer VALUES ("cus_A03", "ord_03", 
+-- (SELECT user_id FROM user WHERE user_id="user_11")
+-- );
+-- SELECT * FROM customer;
+
+
 -- Inserting record for customer_order table
+-- INSERT INTO clothingDB.customer_order VALUES ("ord_01", "Oluyole, Ibadan", 2410, "2023-09-03 17:13:51", 
+	-- (SELECT payment_method_id FROM payment_method WHERE payment_method_id="pay_AR7"),
+    -- (SELECT clothing_item_id FROM clothing_item WHERE clothing_item_id="item_41"),
+    -- (SELECT delivery_id FROM delivery_method WHERE delivery_id="del_B19")
+-- );
+
+-- INSERT INTO clothingDB.customer_order VALUES ("ord_03", "Ikeja, Lagos", 7450, "2023-09-02 9:25:05", 
+-- 	  (SELECT payment_method_id FROM payment_method WHERE payment_method_id="pay_AJ5"),
+--    (SELECT clothing_item_id FROM clothing_item WHERE clothing_item_id="item_36"),
+--    (SELECT delivery_id FROM delivery_method WHERE delivery_id="del_A17")
+-- );
+
+-- INSERT INTO clothingDB.customer_order VALUES ("ord_02", "Wuse, Abuja", 5570, "2023-09-02 9:25:05", 
+-- 	 (SELECT payment_method_id FROM payment_method WHERE payment_method_id="pay_AA9"),
+--   (SELECT clothing_item_id FROM clothing_item WHERE clothing_item_id="item_09"),
+--   (SELECT delivery_id FROM delivery_method WHERE delivery_id="del_A03")
+-- );
+-- SELECT * FROM customer_order;
 
 
 -- Inserting record for cashier_payroll table
@@ -66,30 +126,59 @@
 -- show items in cashier_payroll table;
 -- SELECT * FROM cashier_payroll;
 
+-- UPDATE cashier_payroll 	
+-- SET payroll_type="daily", amount_gained=4300
+-- WHERE cashier_payroll_id="cas_001";
+
 
 -- Inserting record for payment_method table
--- INSERT INTO clothingDB.payment_method VALUES ("pay_AR2", 1, 50, '2010-11-9 8:30:30');
--- INSERT INTO clothingDB.payment_method VALUES ("pay_AA3", 0, 0, '2021-05-5 21:49:58');
--- INSERT INTO clothingDB.payment_method VALUES ("pay_AJ4", 1, 50, '2023-06-02 10:33:21');
--- INSERT INTO clothingDB.payment_method VALUES ("pay_AR7", 0, 0, '2019-01-17 14:44:10');
--- INSERT INTO clothingDB.payment_method VALUES ("pay_AA9", 1, 50, '2021-05-5 23:59:15');
--- INSERT INTO clothingDB.payment_method VALUES ("pay_AJ5", 0, 0, '2020-08-25 17:36:55');
+-- INSERT INTO clothingDB.payment_method VALUES ("pay_AR2", "transfer", 50, '2010-11-9 8:30:30');
+-- INSERT INTO clothingDB.payment_method VALUES ("pay_AA3", "cash", 0, '2021-05-5 21:49:58');
+-- INSERT INTO clothingDB.payment_method VALUES ("pay_AJ4", "transfer", 50, '2023-06-02 10:33:21');
+-- INSERT INTO clothingDB.payment_method VALUES ("pay_AR7", "debit_card", 70, '2019-01-17 14:44:10');
+-- INSERT INTO clothingDB.payment_method VALUES ("pay_AA9", "credit_card", 70, '2021-05-5 23:59:15');
+-- INSERT INTO clothingDB.payment_method VALUES ("pay_AJ5", "cash", 0, '2020-08-25 17:36:55');
 -- show items in payment_method table;
 -- SELECT * FROM payment_method;
--- DELETE FROM payment_method where payment_method_id = "pay_AJ5"
+     
+-- DELETE FROM payment_method where payment_method_id = "pay_AA9" -- [done]
+
+-- updated the values in the table
+-- UPDATE payment_method 	
+-- SET payment_method_type="transfer",payment_charges=50
+-- WHERE payment_method_id="pay_AR2";
+
 
 
 -- Inserting record for delivery_method table
--- INSERT INTO clothingDB.delivery_method VALUES ("del_A17", 0, 70, '2010-11-12 10:00:00');
--- INSERT INTO clothingDB.delivery_method VALUES ("del_A03", 1, 20, '2021-05-8 16:09:34');
--- INSERT INTO clothingDB.delivery_method VALUES ("del_B19", 1, 50, '2023-06-07 23:45:49');
+-- INSERT INTO clothingDB.delivery_method VALUES ("del_A17", "shop outlet", 0, '2010-11-12 10:00:00');
+-- INSERT INTO clothingDB.delivery_method VALUES ("del_A03", "pickup station", 20, '2021-05-8 16:09:34');
+-- INSERT INTO clothingDB.delivery_method VALUES ("del_B19", "doorstep", 50, '2023-06-07 23:45:49');
 -- show items in delivery_method table
 -- SELECT * FROM delivery_method;
+
+-- updated the values in the table
+-- UPDATE delivery_method 	
+-- SET delivery_type="doorstep",delivery_fee=50
+-- WHERE delivery_id="del_B19";
 
 
 -- Inserting record for user table
 -- INSERT INTO clothingDB.user VALUES ("user_07", "Oghenekaro", "customer", "oghenekaro57@gmail.com");
 -- INSERT INTO clothingDB.user VALUES ("user_15", "Emmanuella", "cashier", "oghenekaro27@gmail.com");
 -- INSERT INTO clothingDB.user VALUES ("user_71", "Ellahhh", "admin", "oghenekaro571@gmail.com");
+-- INSERT INTO clothingDB.user VALUES ("user_28", "Rasheed", "admin", "rasheed@gmail.com");
+-- INSERT INTO clothingDB.user VALUES ("user_52", "Daniel", "admin", "daniel@altschoolafrica.com");
+-- INSERT INTO clothingDB.user VALUES ("user_11", "Tabby", "customer", "oghenekaro571@gmail.com");
+-- INSERT INTO clothingDB.user VALUES ("user_90", "Stephen", "customer", "stephen@gmail.com");
+-- INSERT INTO clothingDB.user VALUES ("user_37", "Jerry", "cashier", "jerry@gmail.com");
+-- INSERT INTO clothingDB.user VALUES ("user_02", "banji", "cashier", "banji@gmail.com");
+INSERT INTO clothingDB.user VALUES (user02, "banji", "cashier", "banji@gmail.com");
+
 -- show items in user table
 -- SELECT * FROM user;
+ 
+-- updated the values in the table
+-- UPDATE user 	
+-- SET users_role="customer",user_email="tabby@gmail.com"
+-- WHERE user_id="user_11";
